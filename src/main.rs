49018737973a597b19faac2a498396b74c7ed9f6
bandrefilipe <b.andrefilipe@gmail.com>
2022-03-08 22:23:53 -0300

    Rename package

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::{env, process};
-use rust_binary_template::run_application;
+use ip_sniffer::run_application;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    process::exit(match run_application(&args) {
+    process::exit( match run_application(&args) {
         Ok(_) => 0,
         Err(_) => 1,
     });
