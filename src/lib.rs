fc689a73b90d16e03c7b89f743d0ab7bcfe9b3a4
bandrefilipe <b.andrefilipe@gmail.com>
2022-03-14 22:30:19 -0300

    Move the service code to the appropriate module

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 mod notification;
+mod scan;
 
+use crate::notification::{NotificationFactory, NotificationProducer};
+use crate::scan::ScannerService;
 use clap::Parser;
 use std::error::Error;
-use std::net::{IpAddr, TcpStream};
+use std::net::IpAddr;
 use std::thread;
-use crate::notification::{NotificationFactory, NotificationProducer};
 
 pub fn run_application() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
@@ -25,49 +27,6 @@ pub fn run_application() -> Result<(), Box<dyn Error>> {
     consumer.print_result();
 
     Ok(())
-}
-
-struct ScannerService {
-    notifier: NotificationProducer,
-    address: IpAddr,
-    start_port: u16,
-    increment: u16,
-}
-
-impl ScannerService {
-    fn from(
-        notifier: NotificationProducer,
-        address: IpAddr,
-        start_port: u16,
-        increment: u16,
-    ) -> ScannerService {
-        ScannerService {
-            notifier,
-            address,
-            start_port,
-            increment,
-        }
-    }
-
-    /// Loops through each port, from `start_port` up to [u16::MAX], incrementing the port number by
-    /// the `increment` value in each iteration, and sends each connected port number through the
-    /// channel.
-    fn scan(&self) {
-        let mut port = self.start_port;
-        loop {
-            match TcpStream::connect((self.address, port)) {
-                Ok(_) => self.notifier.connection_succeeded(port),
-                Err(_) => self.notifier.connection_failed(port),
-            }
-
-            if (u16::MAX - port) <= self.increment {
-                // breaks from the loop if incrementing the port would exceed the max value for u16
-                break;
-            }
-
-            port += self.increment;
-        }
-    }
 }
 
 /// A simple IP Sniffer tool written in Rust
---
+++ src/scan.rs
@@ -0,0 +1,46 @@
+use crate::NotificationProducer;
+use std::net::{IpAddr, TcpStream};
+
+/// Responsible for scanning the open ports of an IP address.
+pub struct ScannerService {
+    notifier: NotificationProducer,
+    address: IpAddr,
+    start_port: u16,
+    increment: u16,
+}
+
+impl ScannerService {
+    /// Creates a new `ScannerService` from the given inputs.
+    pub fn from(
+        notifier: NotificationProducer,
+        address: IpAddr,
+        start_port: u16,
+        increment: u16,
+    ) -> ScannerService {
+        ScannerService {
+            notifier,
+            address,
+            start_port,
+            increment,
+        }
+    }
+
+    /// Loops through each port, from `start_port` up to [u16::MAX], incrementing the port number by
+    /// the `increment` value in each iteration, and notifies the progression through the `notifier`.
+    pub fn scan(&self) {
+        let mut port = self.start_port;
+        loop {
+            match TcpStream::connect((self.address, port)) {
+                Ok(_) => self.notifier.connection_succeeded(port),
+                Err(_) => self.notifier.connection_failed(port),
+            }
+
+            if (u16::MAX - port) <= self.increment {
+                // breaks from the loop if incrementing the port would exceed the max value for u16
+                break;
+            }
+
+            port += self.increment;
+        }
+    }
+}
